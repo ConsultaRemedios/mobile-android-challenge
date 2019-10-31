748c05065852e72f6f89cd04fdedd8a7ce8e978b
Marcos Rosa <r2marcos@gmail.com>
2019-10-31 00:47:28 -0300

    programação da GameDetailActivity

--- app/src/main/java/com/challange/crandroid/GameDetailActivity.kt
+++ app/src/main/java/com/challange/crandroid/GameDetailActivity.kt
@@ -1,20 +1,18 @@
 package com.challange.crandroid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.challange.crandroid.adapter.GameDetailImageSlider
 import com.challange.crandroid.api.GameCheckoutServiceInitializer
 import com.challange.crandroid.data.response.GameDetails
-import com.opensooq.pluto.listeners.OnItemClickListener
-import com.skyhope.showmoretextview.ShowMoreTextView
-import io.github.bffcorreia.fole.Fole
+import com.challange.crandroid.utils.GenericUtils.Companion.brazilianNumberFormat
 import kotlinx.android.synthetic.main.activity_game_detail.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class GameDetailActivity : AppCompatActivity() {
 
@@ -22,43 +20,58 @@ class GameDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_detail)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        gameDescriptionToggleViewExpand.setOnClickListener(expandDescription)
+        gameDescriptionToggleViewCollapse.setOnClickListener(collapseDescription)
+        buttonAdicionarCarrinho.setOnClickListener(addToCart)
+
+        // Perform collapse function to start activity with game description collapsed
+        gameDescriptionToggleViewCollapse.performClick()
 
         loadGame(intent.getIntExtra("gameId", 0))
-
-
-        buttonAdicionarCarrinho.setOnClickListener(clickButtonAdicionarCarrinho)
-//        text_view_show_more.setShowingLine(7)
-//        text_view_show_more.addShowMoreText("Ler Mais")
-//        text_view_show_more.addShowLessText("Ler Menos")
     }
 
-    val clickButtonAdicionarCarrinho = View.OnClickListener {
+    private val expandDescription = View.OnClickListener {
+        gameDescriptionTextView.maxLines = Int.MAX_VALUE
+        gameDescriptionToggleViewExpand.visibility = View.GONE
+        gameDescriptionToggleViewCollapse.visibility = View.VISIBLE
+    }
+
+    private val collapseDescription = View.OnClickListener {
+        gameDescriptionTextView.maxLines = 6
+        gameDescriptionToggleViewExpand.visibility = View.VISIBLE
+        gameDescriptionToggleViewCollapse.visibility = View.GONE
+    }
+
+    private val addToCart = View.OnClickListener {
         val intent = Intent(this, CheckoutActivity::class.java)
         startActivity(intent)
     }
 
+    private fun updateViewWithGameDetails(gameDetails: GameDetails) {
+        val images = gameDetails.images as MutableList<String>
+        val adapter = GameDetailImageSlider(images)
+        plutoGameDetails.create(adapter, lifecycle = lifecycle)
+        plutoGameDetails.setCustomIndicator(custom_indicator)
+
+        gameTitle.text = gameDetails.name
+        gameDescriptionTextView.text = gameDetails.description
+        priceTag.text = brazilianNumberFormat().format(gameDetails.price)
+
+        supportActionBar?.title = gameDetails.platform // usar extras
+    }
+
     private fun loadGame(gameId: Int) {
         GameCheckoutServiceInitializer().gameCheckoutService().getGameDetails(gameId)
-            .enqueue(object: Callback<GameDetails> {
+            .enqueue(object : Callback<GameDetails> {
                 override fun onResponse(call: Call<GameDetails>, response: Response<GameDetails>) {
-                    val gameDetail = response.body()
-
-                    val images = gameDetail?.images as MutableList<String>
-                    val adapter = GameDetailImageSlider(images)
-                    plutoGameDetails.create(adapter, lifecycle = lifecycle)
-                    plutoGameDetails.setCustomIndicator(custom_indicator)
-
-
-                    game_title.text = gameDetail.name
-
-                    Fole.with(expandableTextView)
-                        .text(gameDetail.description)
-                        .maxLines(6)
-                        .toggleView(toggle_view)
-
-//                    expandable_text.text = gameDetail.description
+                    if (response.isSuccessful) {
+                        updateViewWithGameDetails(response.body()!!)
+                    } else {
+                        TODO("Notify can't load games")
+                    }
                 }
 
                 override fun onFailure(call: Call<GameDetails>, t: Throwable) {
