3529283c2e466541f1c5db1a227dfff8fdd269fb
Lunider <luniderelias@hotmail.com>
2019-11-01 14:54:44 -0300

    Image Slider Added
    Image Slider from https://github.com/denzcoskun/ImageSlideshow/tree/master/imageslider

--- app/src/main/java/teste/exemplo/com/gamecommerce/MainActivity.kt
+++ app/src/main/java/teste/exemplo/com/gamecommerce/MainActivity.kt
@@ -2,11 +2,20 @@ package teste.exemplo.com.gamecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_game)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.game_super_mario_odyssey))
+        imageList.add(SlideModel(R.drawable.game_super_mario_odyssey))
+        imageList.add(SlideModel(R.drawable.game_super_mario_odyssey))
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
     }
 }
