967f1d2117d0f0c4e815ed84bd39704b7ec37f56
Weslley Maciel <weslley.maciel.c@gmail.com>
2020-08-07 19:18:26 -0300

    Setup project and dependencies

---
+++ games_ecommerce/app/src/main/java/br/com/weslleymaciel/gamesecommerce/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.weslleymaciel.gamesecommerce.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.weslleymaciel.gamesecommerce.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
